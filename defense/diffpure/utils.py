ad7ab2ae5259f06046c07be8fe7f4367c94ba4b4
苏格拉底金 <42350897+Zhijin-Ge@users.noreply.github.com>
2024-09-06 09:08:42 +0800

    Create utils.py

---
+++ defense/diffpure/utils.py
@@ -0,0 +1,150 @@
+# ---------------------------------------------------------------
+# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+#
+# This work is licensed under the NVIDIA Source Code License
+# for DiffPure. To view a copy of this license, see the LICENSE file.
+# ---------------------------------------------------------------
+
+import sys
+import argparse
+from typing import Any
+
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import timm
+
+def compute_n_params(model, return_str=True):
+    tot = 0
+    for p in model.parameters():
+        w = 1
+        for x in p.shape:
+            w *= x
+        tot += w
+    if return_str:
+        if tot >= 1e6:
+            return '{:.1f}M'.format(tot / 1e6)
+        else:
+            return '{:.1f}K'.format(tot / 1e3)
+    else:
+        return tot
+
+
+class Logger(object):
+    """
+    Redirect stderr to stdout, optionally print stdout to a file,
+    and optionally force flushing on both stdout and the file.
+    """
+
+    def __init__(self, file_name: str = None, file_mode: str = "w", should_flush: bool = True):
+        self.file = None
+
+        if file_name is not None:
+            self.file = open(file_name, file_mode)
+
+        self.should_flush = should_flush
+        self.stdout = sys.stdout
+        self.stderr = sys.stderr
+
+        sys.stdout = self
+        sys.stderr = self
+
+    def __enter__(self) -> "Logger":
+        return self
+
+    def __exit__(self, exc_type: Any, exc_value: Any, traceback: Any) -> None:
+        self.close()
+
+    def write(self, text: str) -> None:
+        """Write text to stdout (and a file) and optionally flush."""
+        if len(text) == 0: # workaround for a bug in VSCode debugger: sys.stdout.write(''); sys.stdout.flush() => crash
+            return
+
+        if self.file is not None:
+            self.file.write(text)
+
+        self.stdout.write(text)
+
+        if self.should_flush:
+            self.flush()
+
+    def flush(self) -> None:
+        """Flush written text to both stdout and a file, if open."""
+        if self.file is not None:
+            self.file.flush()
+
+        self.stdout.flush()
+
+    def close(self) -> None:
+        """Flush, close possible files, and remove stdout/stderr mirroring."""
+        self.flush()
+
+        # if using multiple loggers, prevent closing in wrong order
+        if sys.stdout is self:
+            sys.stdout = self.stdout
+        if sys.stderr is self:
+            sys.stderr = self.stderr
+
+        if self.file is not None:
+            self.file.close()
+
+
+def dict2namespace(config):
+    namespace = argparse.Namespace()
+    for key, value in config.items():
+        if isinstance(value, dict):
+            new_value = dict2namespace(value)
+        else:
+            new_value = value
+        setattr(namespace, key, new_value)
+    return namespace
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def update_state_dict(state_dict, idx_start=9):
+
+    from collections import OrderedDict
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = k[idx_start:]  # remove 'module.0.' of dataparallel
+        new_state_dict[name]=v
+
+    return new_state_dict
+
+def wrap_model(model):
+    """
+    Add normalization layer with mean and std in training configuration
+    """
+    if hasattr(model, 'default_cfg'):
+        """timm.models"""
+        mean = model.default_cfg['mean']
+        std = model.default_cfg['std']
+    else:
+        """torchvision.models"""
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+    normalize = transforms.Normalize(mean, std)
+    return torch.nn.Sequential(normalize, model)
+
+def get_image_classifier(model_name):
+    if model_name in models.__dict__.keys():
+        print('=> Loading model {} from torchvision.models'.format(model_name))
+        model = models.__dict__[model_name](weights="DEFAULT")
+    elif model_name in timm.list_models():
+        print('=> Loading model {} from timm.models'.format(model_name))
+        model = timm.create_model(model_name, pretrained=True)
+    else:
+        raise ValueError('Model {} not supported'.format(model_name))
+    return wrap_model(model.eval())
