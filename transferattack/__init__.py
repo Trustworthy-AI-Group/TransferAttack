4d469ddb9894f6238c48e12e0e9cec4f6953bbc1
Nexus <nexusbhliu@gmail.com>
2025-11-18 15:05:33 +0800

    Add DSVA to generation options

--- transferattack/__init__.py
+++ transferattack/__init__.py
@@ -132,6 +132,7 @@ attack_zoo = {
     'ada': ('.generation.ada', 'ADA'),
     'ge_advgan': ('.generation.ge_advgan', 'GE_ADVGAN'),
     'diffattack': ('.generation.diffattack', 'DiffAttack'),
+    'dsva': ('.generation.dsva', 'DSVA'),
     ## Targeted
     'ttp': ('.generation.ttp', 'TTP'),
     'm3d': ('.generation.m3d', 'M3D'),
